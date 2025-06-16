614a5898c0c61ddcac3f53b5d8d0ce92ec26c20e
Dardranight7 <dardranight@gmail.com>
2025-06-16 08:21:20 -0500

    Visual Improvements

--- Assets/_Zombies/Scripts/FirstPersonPointer.cs
+++ Assets/_Zombies/Scripts/FirstPersonPointer.cs
@@ -1,16 +1,90 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstPersonPointer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static System.Action<PointableObject> OnObjectPointed;
+    private PointableObject currentPointedObject;
+    public Image pointerImage; // UI Image to represent the pointer
+
+    private void Awake()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+        Cursor.visible = false; // Hide the cursor
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        RayCastInMiddleOfScreen(); // Cast a ray from the center of the screen
+        //Function to handle input for clicking on the pointed object
+        if (Input.GetMouseButtonDown(0)) // Left mouse button click
+        {
+            if (currentPointedObject != null)
+            {
+                currentPointedObject.OnPointerClick(); // Invoke the click event on the pointed object
+            }
+        }
+    }
+
+    public void RayCastInMiddleOfScreen()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f)) // Adjust the distance as needed
+        {
+            PointableObject pointableObject = hit.collider.GetComponent<PointableObject>();
+            if (pointableObject != null)
+            {
+                OnObjectPointed?.Invoke(pointableObject);
+            }
+            else
+            {
+                OnObjectPointed?.Invoke(null);
+            }
+        }
+        else
+        {
+            OnObjectPointed?.Invoke(null);
+        }
+    }
+
+    private void Start()
+    {
+        OnObjectPointed += HandleObjectPointed;
+    }
+
+    private void OnDestroy()
+    {
+        OnObjectPointed -= HandleObjectPointed;
+    }
+
+    public void HandleObjectPointed(PointableObject pointableObject)
+    {
+        // Handle the object pointed event here
+        if (pointableObject == null)
+        {
+            if (pointerImage != null)
+            {
+                currentPointedObject = null;
+                // Cancela cualquier tween activo en este objeto
+                LeanTween.cancel(pointerImage.gameObject);
+                pointerImage.transform.LeanScale(new Vector3(1, 1, 1), 0.1f);
+            }
+        }
+        else
+        {
+            // If the object is not null, update the pointer image and scale it and difrentiate it from the previous one
+            if (currentPointedObject != pointableObject)
+            {
+                Debug.Log("Pointed at: " + pointableObject.name); 
+                currentPointedObject = pointableObject;
+                if (pointerImage != null)
+                {
+                    // Cancela cualquier tween activo en este objeto
+                    LeanTween.cancel(pointerImage.gameObject);
+                    pointerImage.transform.LeanScale(new Vector3(3, 3, 3),0.1f);
+                }
+            }
+        }
     }
 }
--- Assets/_Zombies/Scripts/PointableObject.cs
+++ Assets/_Zombies/Scripts/PointableObject.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class PointableObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public UnityEvent onClicked; // UnityEvent to handle pointer events
+    public UnityEvent<bool> onClickToToggle;
+    bool lastState = false; // Track the last state for toggling
+    public void OnPointerClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Handle pointer click event here if needed
+        Debug.Log("PointableObject clicked: " + gameObject.name);
+        onClicked?.Invoke(); // Invoke the UnityEvent when clicked
+        onClickToToggle?.Invoke(lastState); // Toggle the state
+        lastState = !lastState; // Update the last state
     }
 }
