0a747a8d0f6bd3897671936d5c7fa3adbabaacb4
Luisa Leaccott <Luisaleaccott5@gmail.com>
2025-06-16 22:27:31 -0500

    update cosas locas de movimiento

--- Assets/_Zombies/Scripts/PlayerController.cs
+++ Assets/_Zombies/Scripts/PlayerController.cs
@@ -1,23 +1,66 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public static PlayerController Instance { get; private set; } // Singleton instance of PlayerController
+    public static PlayerController Instance { get; private set; }
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f; // Movement speed multiplier
+
+    private Rigidbody rb; // Rigidbody reference
+    private InputManager inputActions; // Input system class
+    private Vector2 movementInput; // Store movement input (X, Z)
 
     private void Awake()
     {
-        Instance = this; // Set the singleton instance
+        // Singleton pattern
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        // Initialize input actions
+        inputActions = new InputManager();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnEnable()
     {
-        
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMovePerformed;
+        inputActions.Player.Move.canceled += OnMoveCanceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        inputActions.Player.Move.performed -= OnMovePerformed;
+        inputActions.Player.Move.canceled -= OnMoveCanceled;
+        inputActions.Player.Disable();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the player using Rigidbody physics
+        Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
+        Vector3 velocity = moveDirection * moveSpeed;
+        Vector3 currentVelocity = rb.linearVelocity;
+        rb.linearVelocity = new Vector3(velocity.x, currentVelocity.y, velocity.z);
+    }
+
+    // Called when movement input is performed
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    // Called when movement input is released
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        movementInput = Vector2.zero;
     }
 }
+
